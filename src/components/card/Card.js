bdcf75e37fb7395ddfb2c827fdbeb95eb32f1d0f
Ata-92 <atakal92@gmail.com>
2021-05-09 04:04:52 +0300

    Add category object and map method

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Card.css";
-
+import {categories} from "../../helper/data";
 
 class Card extends Component {
 
@@ -9,7 +9,12 @@ class Card extends Component {
     return (
       <div className="Card">
         <h1>Languages</h1>
-
+        {categories.map((category) => (
+          <div className={category.name}>
+            <img src={category.img} alt={`${category.name} logo`} className={`${category.name}-img`} />
+            <p>{category.name}</p>
+          </div>
+        ))}
       </div>
     )
   }
