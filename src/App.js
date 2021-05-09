70f6217dbb77cdf4c0e5d866845f53fb11c3e75a
Ata-92 <atakal92@gmail.com>
2021-05-09 03:56:25 +0300

    Add image and a css file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import react from "./assets/react.svg";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <img src={react} alt="React Logo" width="20%"/>
 
+      </div>
     </div>
   );
 }
