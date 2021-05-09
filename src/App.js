fd4b00ff6d94b766e72f43e289959346b679472d
Ata-92 <atakal92@gmail.com>
2021-05-09 04:02:40 +0300

    Add Card component and its styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import react from "./assets/react.svg";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <img src={react} alt="React Logo" width="20%"/>
-
+        <Card />
       </div>
     </div>
   );
